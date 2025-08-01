6094281c3a64d7c5f8c83185602f6ed870615aaa
Alexandr <oleksandrglushen2005@gmail.com>
2025-08-01 16:55:41 +0300

    fixed code

---
+++ taxi/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.core.exceptions import ValidationError
+import re
+
+from .models import Driver, Car
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Driver
+        fields = ['license_number']
+
+    def clean_license_number(self):
+        licence_number = self.cleaned_data['license_number']
+
+        pattern = r"^[A-Z]{3}[0-9]{5}$"
+
+        if not re.match(pattern, licence_number):
+            raise ValidationError("License number must consist of 8 characters: "
+                "first 3 uppercase letters, followed by 5 digits.")
+        return licence_number
+
+
+class DriverCreationForm(forms.ModelForm):
+    class Meta:
+        model = Driver
+        fields = ("username", "first_name", "last_name", "license_number")
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        pattern = r"^[A-Z]{3}[0-9]{5}$"
+        if not re.match(pattern, license_number):
+            raise ValidationError(
+                "License number must consist of 8 characters: "
+                "first 3 uppercase letters, followed by 5 digits."
+            )
+        return license_number
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(queryset=Driver.objects.all(),
+                                             widget=forms.CheckboxSelectMultiple,
+                                             required=False)
+    class Meta:
+        model = Car
+        fields = "__all__"
--- taxi/views.py
+++ taxi/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import DriverLicenseUpdateForm, DriverCreationForm, CarForm
 
 from .models import Driver, Car, Manufacturer
 
@@ -26,6 +27,17 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+@login_required
+def toggle_assign_to_car(request, pk):
+    car = get_object_or_404(Car, pk=pk)
+
+    if request.user in car.drivers.all():
+        car.drivers.remove(request.user)
+    else:
+        car.drivers.add(request.user)
+
+    return redirect("taxi:car-detail", pk=pk)
 
 
 class ManufacturerListView(LoginRequiredMixin, generic.ListView):
@@ -64,7 +76,7 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
 
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
-    fields = "__all__"
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
@@ -87,3 +99,23 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+
+
+class DriverCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Driver
+    form_class = DriverCreationForm
+    success_url = reverse_lazy("taxi:driver-list")
+    template_name = "taxi/driver_form.html"
+
+
+class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Driver
+    success_url = reverse_lazy("taxi:driver-list")
+    template_name = "taxi/driver_confirm_delete.html"
+
+
+class DriverUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Driver
+    form_class = DriverLicenseUpdateForm
+    success_url = reverse_lazy("taxi:driver-list")
+    template_name = "taxi/driver_form.html"
